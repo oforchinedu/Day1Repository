84640de961bc62e50e20c7eae29743d9a18e1df2
chinedu ofor <oforchinedu12@gmail.com>
2017-01-09 04:17:07 +0100

    final commit

--- tests/testPrime.js
+++ tests/testPrime.js
@@ -12,11 +12,17 @@ describe("Prime list tests ", function(){
 		it("it should return false for 0", function(){
 			expect(primeList(1)).toEqual(false);
 		})
+	})
+
+	describe("case for negative numbers", function(){
 
 		it("it should return false for -1", function(){
 			expect(primeList(-1)).toEqual(false);
 		})
 
+		it("it should return false for -2", function(){
+			expect(primeList(-2)).toEqual(false);
+		})
 	})
 
 	describe("Case for the number 2", function(){
@@ -34,6 +40,18 @@ describe("Prime list tests ", function(){
 
 		it("it should return [ 2, 3, 5, 7, 11, 13, 17, 19 ] for 20 ", function(){
 			expect(primeList(20)).toEqual([ 2, 3, 5, 7, 11, 13, 17, 19 ]);
+		})
+
+		it("it should return [ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 ] for 30", function(){
+			expect(primeList(30)).toEqual([ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 ]);
+		})
+
+		it("it should return [2,3,5,7] for 40", function(){
+			expect(primeList(40)).toEqual([ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37 ]);
+		})
+
+		it("it should return [ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47 ] for 50", function(){
+			expect(primeList(50)).toEqual([ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47 ]);
 		})
 	})
 
