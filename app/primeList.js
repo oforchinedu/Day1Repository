56eefd676ada381f8df94e05368bb02baeae0888
chinedu ofor <oforchinedu12@gmail.com>
2017-01-09 03:43:54 +0100

    Added PrimeList function

---
+++ app/primeList.js
@@ -0,0 +1,19 @@
+module.exports = {
+  isPrime: function(value) {
+    for(var i = 2; i < value; i++) {
+        if(value % i === 0) {
+            return false;
+        }
+    }
+    return value > 1;
+  }
+}
+
+function isPrime(value) {
+    for(var i = 2; i < value; i++) {
+        if(value % i === 0) {
+            return false;
+        }
+    }
+    return value > 1;
+}
