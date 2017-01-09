cfb23011cf006899020f56edf4a7063bb2c00869
chinedu ofor <oforchinedu12@gmail.com>
2017-01-09 03:54:21 +0100

    I added a test for prime function wrote 2 tests

--- tests/testPrime.js
+++ tests/testPrime.js
@@ -1,3 +1,27 @@
 'use strict'
 
-var isPrime = require('../app/testPrime.js').isPrime
+var primeList = require('../app/testPrime.js').primeList
+
+describe("Prime list tests ", function(){
+	describe("Case for values less than two", function(){
+
+		it("it should return false for 1", function(){
+			expect(primeList(1)).toEqual(false);
+		})
+
+		it("it should return false for 0", function(){
+			expect(primeList(1)).toEqual(false);
+		})
+
+	})
+
+
+
+
+
+
+
+
+
+
+})
